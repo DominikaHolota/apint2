5a9421ee7eebe0ef93a181859147fc28167f1361
Dominika Holota <atthis@o2.pl>
2018-07-01 17:28:39 +0200

    Add about page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var nodemailer = require('nodemailer');
 var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
+var about = require ('./routes/about');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', indexRouter);
+app.use('/about',about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
