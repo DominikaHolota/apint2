78cf916a8cffff014d3f6141f2a801bec8161c45
Dominika Holota <atthis@o2.pl>
2018-07-01 15:55:12 +0200

    fix stylesheets

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 
@@ -18,6 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 app.use('/', indexRouter);
 
